292f5db133cc87df360f44ff0e378bffdc1a304f
Kholoud Fattem <120126262+Kholoudxs55kh@users.noreply.github.com>
2023-09-25 02:52:55 +0300

    test

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 # using Puppet to make changes to our configuration file
 
 file_line {'Change File'
-path   => './2-ssh_config',
+path   => '/etc/ssh/ssh_config',
 ensure => 'present',
 line   => 'IdentityFile ~/.ssh/school',
 match  => '#   IdentityFile ~/.ssh/id_rsa',
 }
 
 file_line {'Change File'
-path   => './2-ssh_config',
+path   => '/etc/ssh/ssh_config',
 ensure => 'present',
 line   => 'PasswordAuthentication yes',
 match  => '#   PasswordAuthentication no',
